a1c64aca172b293f67f29e23bed8fd1ed652299a
chunky123 <me@chengpeng.ca>
2015-12-31 21:39:17 -0500

    Added wiki table

--- private/pgscript.sql
+++ private/pgscript.sql
@@ -12,3 +12,26 @@ CREATE TABLE historical(
 	adjClose 	real NOT NULL,
 	volume 		integer NOT NULL
 );
+
+CREATE TABLE historical_wiki(
+	symbol 		char(10),
+	date 		date,
+	open 		real,
+	high 		real,
+	low 		real,
+	close 		real,
+	volume 		integer,
+	ex_dividend real,
+	split_ratio real,
+	adj_open 	real,
+	adj_high 	real,
+	adj_low  	real,
+	adj_close 	real,
+	adj_volume 	real
+);
+
+CREATE TABLE symbols_wiki(
+	symbol  char(10)
+);
+INSERT INTO symbols_wiki
+SELECT DISTINCT symbol FROM historical_wiki ORDER BY symbol ASC;
